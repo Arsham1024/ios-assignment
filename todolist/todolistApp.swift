fdf6a1f7a2fa5305e7756ddee5154b7eb3cbc7ba
Arsham1024 <Arsham512@gmail.com>
2023-11-05 13:54:11 -0800

    Initial Commit

---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by Arsham Mehrani on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
